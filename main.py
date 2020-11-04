eb5d075cf8ea9f6d5a0cc2b51e9afecc60816d37
Your Name <you@example.com>
2020-11-04 09:38:46 +0000

    Make network node

---
+++ main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+class configs:
+    # velocity of vehicle
+    asdf
+    asdf
+    asdf
+    asdf
+    asdf
+
---
+++ network.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+class node:
+    def __init__(self, x=0, y=0, start_time=0, end_time):
+        self.x = x
+        self.y = y
+        self.start_time = start_time
+        self.end_time = end_time
+
+class vehicle:
+
+
+
