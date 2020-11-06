46d59fb2b68a0e3a2904c38d267602ad168fd725
hslyu <hslyu@unist.ac.kr>
2020-11-06 13:26:38 +0900

    Add trajectory finding module to classic TSP problem

--- tsp.py
+++ tsp.py
@@ -18,16 +18,17 @@ dist = [
 
 # Cache and route are indexed by bit masking
 cache = [[None for _ in range(1<<n)] for _ in range(n)]
-route = [[ -1 for _ in range(1<<n)] for _ in range(n)]
+next_node = [[ -1 for _ in range(1<<n)] for _ in range(n)]
 
 INF = 99999
+start=0
 
 def shortest_path2(cur, visited):
     # base case
     if visited == (1<<n)-1:
         ##
-        route[cur][visited-1] = 0
-        return dist[cur][0]
+        next_node[cur][visited-1] = start
+        return dist[cur][start]
 
     if cache[cur][visited-1] != None:
         return cache[cur][visited-1]
@@ -36,12 +37,32 @@ def shortest_path2(cur, visited):
     best=-1
     for _next in range(n):
         if not (visited & (1<<_next)):
+            prev=cache[cur][visited-1]
             cache[cur][visited-1] = \
             min(cache[cur][visited-1], dist[cur][_next]+shortest_path2(_next, visited+(1<<_next)))
+            if prev!=cache[cur][visited-1]:
+                next_node[cur][visited-1]=_next
 
     return cache[cur][visited-1]
 
 if __name__ == "__main__":
+    assert start<n, "Initial node index should be lower than total number of node - 1"
     #pdb.set_trace()
-    print(shortest_path2(0, 1<<0))
+    print(shortest_path2(start, 1<<start))
     print(cache)
+    
+    print(next_node)
+    route=[]
+    route.append(start)
+
+    cur=start
+    mask=1<<start
+    while mask != (1<<n)-1:
+        print("mask is:",mask)
+        dest = next_node[cur][mask-1]
+        print("next node is:", next_node[cur][mask-1])
+        route.append(dest)
+        mask += 1<<dest
+        cur=dest
+
+    print(route)
