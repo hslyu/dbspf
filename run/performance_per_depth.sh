fce429995871e4323f372cfc0db298770464a912
hslyu <hslyu4@gmail.com>
2021-07-21 16:13:16 +0900

    Replace $1, $2 ,$3 to specific bash variables

--- run/performance_per_depth.sh
+++ run/performance_per_depth.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-if [ -z $1 ]
+if [ $# -lt 3 ]
 then
-	echo "Depth should be specified. Usage: $0 {DEPTH} {index_start} {index_end}"
+	echo "Depth, index_start and index_end should be specified. Usage: $0 {DEPTH} {index_start} {index_end}"
 	exit
 fi
+
+DEPTH="$1"
+INDEX_START="$2"
+INDEX_END="$3"
 
 if [ ! -d './result' ]
 then
 	mkdir result
 fi
 
+
 for i in {2..11}
 do
 	num_user=$(($i * 5))
-	result_dir="./result/result-depth_$1-user_$num_user"
-	python main.py -t $1 --num_user $num_user --result_path $result_dir --index_start $2 --index_end $3
-	echo "Computation for depth $1 and user $num_user is done."
+	result_dir="./result/result-depth_$DEPTH-user_$num_user"
+	python main.py -t $DEPTH --num_user $num_user --result_path $result_dir --index_start $INDEX_START --index_end $INDEX_END
+	echo "Computation for depth $DEPTH and user $num_user is done."
 	echo "----------------------------------------------------"
 done
