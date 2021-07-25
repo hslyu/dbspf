88ab22ad499c430cc45cdcce001de0e665ca8e0b
hslyu <hslyu4@gmail.com>
2021-07-26 04:24:44 +0900

    Troubleshoot: add num_user variable

--- run/run_main.sh
+++ run/run_main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+num_user=$4
 result_dir="./result/result-depth_$1-user_$num_user"
-python main.py -t $1 --index_start $2 --index_end $3 --num_user $4 --result_path $result_dir
+python main.py -t $1 --index_start $2 --index_end $3 --num_user $num_user --result_path $result_dir
