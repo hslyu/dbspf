de3673b6f830d40af2710f5da6b68c11cad7d504
hslyu <hslyu4@gmail.com>
2021-07-29 05:58:03 +0900

    Add datarate model, enhence code

--- analysis/performance_per_datarate.py
+++ analysis/performance_per_datarate.py
@@ -14,116 +14,116 @@ import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
-from utils import open_json
+from utils import create_dir
 
-dir_path = 'some-value_per_datarate/'
+def open_json(file_path):
+    with open(file_path, encoding='utf-8') as f:
+        result = json.load(f)
+        time = result['total_time']
+        reward = result['total_reward']
+        user_list = result['trajectory'][-1]['user_list']
+        total_data = sum([user['total_data'] for user in user_list])
+        total_data_square = sum([user['total_data']**2 for user in user_list])
+        
+        avg_data = total_data/len(user_list)
+        var_data = total_data_square/len(user_list)
+        return time, reward, avg_data, var_data
 
-def plot_per_datarate(depth,dir_path):#{{{
-    with open(dir_path+'time_per_depth.pkl', 'rb') as f:
-        time_per_depth = pickle.load(f)
-    with open(dir_path+'reward_per_depth.pkl', 'rb') as f:
-        reward_per_depth = pickle.load(f)
-    with open(dir_path+'avg_data_per_depth.pkl', 'rb') as f:
-        avg_data_per_depth = pickle.load(f)
-    with open(dir_path+'var_data_per_depth.pkl', 'rb') as f:
-        var_data_per_depth = pickle.load(f)
+def plot_per_datarate(pkl_dir_path, datarate_list):
+    with open(pkl_dir_path+'avg_time_per_step_per_datarate.pkl', 'rb') as f:
+        avg_time_per_step_per_datarate = pickle.load(f)
+    with open(pkl_dir_path+'avg_reward_per_step_per_datarate.pkl', 'rb') as f:
+        avg_reward_per_step_per_datarate = pickle.load(f)
+    with open(pkl_dir_path+'avg_data_per_datarate.pkl', 'rb') as f:
+        avg_data_per_datarate = pickle.load(f)
+    with open(pkl_dir_path+'var_data_per_datarate.pkl', 'rb') as f:
+        var_data_per_datarate = pickle.load(f)
 
     plot1 = plt.figure(1)
-    for idx in range(depth):
-        plt.plot(num_user_list, time_per_depth[idx], label = f'depth{idx+1}')
-    plt.xlabel("Number of user")
-    plt.ylabel("Time per user")
-    plt.legend()
+    plt.plot(datarate_list, avg_time_per_step_per_datarate)
+    plt.xlabel("Datarate")
+    plt.ylabel("Average time per step")
     
     plot2 = plt.figure(2)
-    for idx in range(depth):
-        plt.plot(num_user_list, 
-                [reward /time for reward, time in zip(reward_per_depth[idx], time_per_depth[idx])],
-                label = f'depth{idx+1}')
-    plt.xlabel("Number of user")
-    plt.ylabel("Total reward per unit time")
-    plt.legend()
+    plt.plot(datarate_list, avg_reward_per_step_per_datarate)
+    plt.xlabel("Datarate")
+    plt.ylabel("Reward per step")
 
     plot3 = plt.figure(3)
-    for idx in range(depth):
-        plt.plot(num_user_list, 
-                avg_data_per_depth[idx],
-                label = f'depth{idx+1}')
-    plt.xlabel("Number of user")
-    plt.ylabel("Avg data per user")
-    plt.legend()
+    plt.plot(datarate_list, 
+            [reward /time for reward, time in zip(avg_reward_per_step_per_datarate, avg_time_per_step_per_datarate)])
+    plt.xlabel("Datarate")
+    plt.ylabel("Avg. reward/Avg. time")
 
     plot4 = plt.figure(4)
-    for idx in range(depth):
-        plt.plot(num_user_list, 
-                var_data_per_depth[idx],
-                label = f'depth{idx+1}')
-    plt.xlabel("Number of user")
-    plt.ylabel("var data per user")
-    plt.legend()
+    plt.plot(datarate_list, avg_data_per_datarate)
+    plt.xlabel("Datarate")
+    plt.ylabel("Average of user received data")
 
     plot5 = plt.figure(5)
-    for idx in range(depth):
-        plt.plot(num_user_list, 
-                [reward/200 for reward in reward_per_depth[idx]],
-                label = f'depth{idx+1}')
-    plt.xlabel("Number of user")
-    plt.ylabel("Reward per depth")
-    plt.legend()
-    plt.show()#}}}
+    plt.plot(datarate_list, var_data_per_datarate)
+    plt.xlabel("Datarate")
+    plt.ylabel("Variance of user received data")
+    plt.show()
+    
 
 if __name__=="__main__":
     # Load data
     num_env = 100
     max_time = 200
-    master_dir = os.path.join(os.getcwd(),'result')
+    depth = 2
+    num_ue = 40
     datarate_list = list(range(5,101,5))
+    # Input and output directory path
+    result_dir = os.path.join(os.getcwd(),'../result_datarate')
+    pkl_dir_path = 'some-value_per_datarate/'
 
     '''
-    time_per_depth = []#{{{
-    reward_per_depth = []
-    avg_data_per_depth = []
-    var_data_per_depth = []
-    for datarate in range(5,101,5):
-        avg_time = []
-        avg_reward = []
-        avg_data = []
-        var_data = []
-        for num_ue in num_user_list:
-            dirname = os.path.join(master_dir,f'result-depth_{depth}-user_{num_ue}')
-            total_time = 0
-            total_reward = 0
-            total_data = 0
-            total_data_square = 0
-            for env_index in range(num_env):
-                filename = f'env_{env_index:04d}-depth_{depth}-ue_{num_ue}.json'
-                result = open_json(os.path.join(dirname,filename))
-                total_time += result['total_time']
-                total_reward += result['total_reward']
-                user_list = result['trajectory'][-1]['user_list']
-                for user in user_list:
-                    total_data += user['total_data']
-                    total_data_square += user['total_data']**2
-            avg_data.append(total_data/num_ue/num_env)
-            var_data.append(total_data_square/num_ue/num_env - (total_data/num_ue/num_env)**2)
+    #{{{
+    avg_time_per_step_per_datarate = []
+    avg_reward_per_step_per_datarate = []
+    avg_data_per_datarate = []
+    var_data_per_datarate = []
+    for datarate in datarate_list:
+        dirname = os.path.join(result_dir,f'datarate_{datarate}')
 
-            avg_time_per_step = total_time/num_env
-            avg_reward_per_step = total_reward/num_env
-            avg_time.append(avg_time_per_step)
-            avg_reward.append(avg_reward_per_step)
-            print(f'Step : [{depth}, {num_ue}],  Time : {avg_time_per_step:.6f},  Reward : {avg_reward_per_step:.2f}')
-        time_per_depth.append(avg_time)
-        reward_per_depth.append(avg_reward)
-        avg_data_per_depth.append(avg_data)
-        var_data_per_depth.append(var_data)
+        avg_time_per_step = 0
+        avg_reward_per_step = 0
+        avg_data = 0
+        var_data = 0
+        for env_idx in range(num_env):
+            # Open file
+            filename = f'env_{env_idx:04d}-depth_{depth}-ue_{num_ue}.json'
+            time, reward, env_avg_data, env_var_data = open_json(os.path.join(dirname,filename))
+
+            # Get average
+            avg_time_per_step += time
+            avg_reward_per_step += reward
+            avg_data += env_avg_data
+            var_data += env_var_data
+            print(f'Environment: [{env_idx+1}/{num_env}] for datarate {datarate}', end='\r', flush=True)
+        print('')
+        # Normalize
+        avg_time_per_step /= num_env*max_time
+        avg_reward_per_step /= num_env*max_time
+        avg_data /= num_env
+        var_data /= num_env
+        # Save
+        avg_time_per_step_per_datarate.append(avg_time_per_step)
+        avg_reward_per_step_per_datarate.append(avg_reward_per_step)
+        avg_data_per_datarate.append(avg_data)
+        var_data_per_datarate.append(var_data)
+
+    create_dir(pkl_dir_path)
     # Save time, reward
-    with open('analysis/time_per_depth.pkl', 'wb') as f:
-        pickle.dump(time_per_depth, f)
-    with open('analysis/reward_per_depth.pkl', 'wb') as f:
-        pickle.dump(reward_per_depth, f)
-    with open('analysis/avg_data_per_depth.pkl', 'wb') as f:
-        pickle.dump(avg_data_per_depth, f)
-    with open('analysis/var_data_per_depth.pkl', 'wb') as f:
-        pickle.dump(var_data_per_depth, f)#}}}
+    with open(pkl_dir_path+'avg_time_per_step_per_datarate.pkl', 'wb') as f:
+        pickle.dump(avg_time_per_step_per_datarate, f)
+    with open(pkl_dir_path+'avg_reward_per_step_per_datarate.pkl', 'wb') as f:
+        pickle.dump(avg_reward_per_step_per_datarate, f)
+    with open(pkl_dir_path+'avg_data_per_datarate.pkl', 'wb') as f:
+        pickle.dump(avg_data_per_datarate, f)
+    with open(pkl_dir_path+'var_data_per_datarate.pkl', 'wb') as f:
+        pickle.dump(var_data_per_datarate, f)
+    #}}}
     '''
-    plot_per_datarate(depth, dir_path)
+    plot_per_datarate(pkl_dir_path, datarate_list)
--- analysis/performance_per_depth.py
+++ analysis/performance_per_depth.py
@@ -14,12 +14,9 @@ import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
-from utils import open_json
+from utils import open_json, create_dir
 
-
-dir_path = 'some-value_per_depth/'
-
-def plot_per_ue(depth,dir_path):#{{{
+def plot_per_ue(dir_path, num_user_list, depth):#{{{
     with open(dir_path+'time_per_depth.pkl', 'rb') as f:
         time_per_depth = pickle.load(f)
     with open(dir_path+'reward_per_depth.pkl', 'rb') as f:
@@ -33,100 +30,111 @@ def plot_per_ue(depth,dir_path):#{{{
     for idx in range(depth):
         plt.plot(num_user_list, time_per_depth[idx], label = f'depth{idx+1}')
     plt.xlabel("Number of user")
-    plt.ylabel("Time per user")
+    plt.ylabel("Time")
     plt.legend()
     
     plot2 = plt.figure(2)
     for idx in range(depth):
-        plt.plot(num_user_list, 
-                [reward /time for reward, time in zip(reward_per_depth[idx], time_per_depth[idx])],
-                label = f'depth{idx+1}')
+        plt.plot(num_user_list, reward_per_depth[idx],label = f'depth{idx+1}')
     plt.xlabel("Number of user")
-    plt.ylabel("Total reward per unit time")
+    plt.ylabel("Reward per time step")
     plt.legend()
 
     plot3 = plt.figure(3)
     for idx in range(depth):
         plt.plot(num_user_list, 
-                avg_data_per_depth[idx],
+                [reward /time for reward, time in zip(reward_per_depth[idx], time_per_depth[idx])],
                 label = f'depth{idx+1}')
     plt.xlabel("Number of user")
-    plt.ylabel("Avg data per user")
+    plt.ylabel("Avg. reward / computation time")
     plt.legend()
 
     plot4 = plt.figure(4)
     for idx in range(depth):
         plt.plot(num_user_list, 
-                var_data_per_depth[idx],
+                avg_data_per_depth[idx],
                 label = f'depth{idx+1}')
     plt.xlabel("Number of user")
-    plt.ylabel("var data per user")
+    plt.ylabel("Average of the user received data")
     plt.legend()
 
     plot5 = plt.figure(5)
     for idx in range(depth):
         plt.plot(num_user_list, 
-                [reward/200 for reward in reward_per_depth[idx]],
+                var_data_per_depth[idx],
                 label = f'depth{idx+1}')
     plt.xlabel("Number of user")
-    plt.ylabel("Reward per depth")
+    plt.ylabel("Variance of the user received data")
     plt.legend()
-    plt.show()#}}}
+    plt.show()
+    #}}}
 
 if __name__=="__main__":
     # Load data
     num_env = 20
     max_time = 200
-    depth = 5
-    master_dir = os.path.join(os.getcwd(),'result')
+    max_depth = 4
     num_user_list = list(range(10,56,5))
 
+    result_dir = os.path.join(os.getcwd(),'../result')
+    pkl_dir_path = 'some-value_per_depth/'
 
     '''
     time_per_depth = []#{{{
     reward_per_depth = []
     avg_data_per_depth = []
     var_data_per_depth = []
-    for depth in range(1,6):
-        avg_time = []
-        avg_reward = []
-        avg_data = []
-        var_data = []
+    for depth in range(1, max_depth+1):
+        avg_time_per_ue = []
+        avg_reward_per_ue = []
+        avg_data_per_ue = []
+        var_data_per_ue = []
         for num_ue in num_user_list:
-            dirname = os.path.join(master_dir,f'result-depth_{depth}-user_{num_ue}')
-            total_time = 0
-            total_reward = 0
-            total_data = 0
-            total_data_square = 0
+            dirname = os.path.join(result_dir,f'result-depth_{depth}-user_{num_ue}')
+            avg_time_per_step = 0
+            avg_reward_per_step = 0
+            avg_data = 0
+            var_data = 0
             for env_index in range(num_env):
+                # Open file
                 filename = f'env_{env_index:04d}-depth_{depth}-ue_{num_ue}.json'
                 result = open_json(os.path.join(dirname,filename))
-                total_time += result['total_time']
-                total_reward += result['total_reward']
+                # Save time, reward result
+                avg_time_per_step += result['total_time']/max_time
+                avg_reward_per_step += result['total_reward']/max_time
+                # Save avg., var.
                 user_list = result['trajectory'][-1]['user_list']
-                for user in user_list:
-                    total_data += user['total_data']
-                    total_data_square += user['total_data']**2
-            avg_data.append(total_data/num_ue/num_env)
-            var_data.append(total_data_square/num_ue/num_env - (total_data/num_ue/num_env)**2)
+                total_data = sum([user['total_data'] for user in user_list])
+                total_data_square = sum([user['total_data']**2 for user in user_list])
+                avg_data += total_data/len(user_list)
+                var_data += total_data_square/len(user_list) - (total_data/len(user_list))**2
+            # Normalize
+            avg_time_per_step /= num_env
+            avg_reward_per_step /= num_env
+            avg_data /= num_env
+            var_data /= num_env
+            
+            avg_time_per_ue.append(avg_time_per_step)
+            avg_reward_per_ue.append(avg_reward_per_step)
+            avg_data_per_ue.append(avg_data)
+            var_data_per_ue.append(var_data)
+            print(f'Step : [{depth}, {num_ue}],  Time : {avg_time_per_step:.6f},  Reward : {avg_reward_per_step:.2f}', 
+                    end='\r', flush=True)
+        print('')
+        time_per_depth.append(avg_time_per_ue)
+        reward_per_depth.append(avg_reward_per_ue)
+        avg_data_per_depth.append(avg_data_per_ue)
+        var_data_per_depth.append(var_data_per_ue)
 
-            avg_time_per_step = total_time/num_env
-            avg_reward_per_step = total_reward/num_env
-            avg_time.append(avg_time_per_step)
-            avg_reward.append(avg_reward_per_step)
-            print(f'Step : [{depth}, {num_ue}],  Time : {avg_time_per_step:.6f},  Reward : {avg_reward_per_step:.2f}')
-        time_per_depth.append(avg_time)
-        reward_per_depth.append(avg_reward)
-        avg_data_per_depth.append(avg_data)
-        var_data_per_depth.append(var_data)
+    create_dir(pkl_dir_path)
     # Save time, reward
-    with open('analysis/time_per_depth.pkl', 'wb') as f:
+    with open(pkl_dir_path+'time_per_depth.pkl', 'wb') as f:
         pickle.dump(time_per_depth, f)
-    with open('analysis/reward_per_depth.pkl', 'wb') as f:
+    with open(pkl_dir_path+'reward_per_depth.pkl', 'wb') as f:
         pickle.dump(reward_per_depth, f)
-    with open('analysis/avg_data_per_depth.pkl', 'wb') as f:
+    with open(pkl_dir_path+'avg_data_per_depth.pkl', 'wb') as f:
         pickle.dump(avg_data_per_depth, f)
-    with open('analysis/var_data_per_depth.pkl', 'wb') as f:
+    with open(pkl_dir_path+'var_data_per_depth.pkl', 'wb') as f:
         pickle.dump(var_data_per_depth, f)#}}}
     '''
-    plot_per_ue(depth, dir_path)
+    plot_per_ue(pkl_dir_path, num_user_list, max_depth)
