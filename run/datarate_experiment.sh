4efd9db3f08083a59559d3062a623bea29063982
hslyu <hslyu4@gmail.com>
2021-08-02 22:25:23 +0900

    make script to run on multiple nodes

--- run/datarate_experiment.sh
+++ run/datarate_experiment.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
-mkdir result_datarate
 NUM_USER=40
-DATARATE_INDEX_START=$1
-DATARATE_INDEX_END=$2
-if [ $# -lt 2 ]
+DATARATE_INDEX_START=$((5*$1))
+DATARATE_INDEX_END=$((DATARATE_INDEX_START+4))
+if [ $# -lt 1 ]
 then
-	echo "Start and end index for datarate should be specified. Usage: $0 <DATARATE_INDEX_START> <DATARATE_INDEX_END>"
+	echo "Session number should be specified. Usage: $0 <DATARATE_INDEX_START> <DATARATE_INDEX_END>"
 	exit
 fi
 
-for i in {1..20}
+for i in $(seq $DATARATE_INDEX_START $DATARATE_INDEX_END)
 do
-	DATARATE=$((i*5))
+	DATARATE=$i
 	echo "Iteration for datarate $DATARATE"
 	python iterative_simulation.py\
 		--tree_depth 2\
-		--env_path $PWD/data_required_datarate\
+		--env_path $PWD/data_datarate_max_data\
 		--env_args_filename args_datarate-$DATARATE.json\
-		--result_path $PWD/result_datarate/datarate_$DATARATE\
+		--result_path $PWD/result_datarate_max_data/datarate_$DATARATE\
 		--num_user $NUM_USER\
 		--index_start 0\
-		--index_end 100\
+		--index_end 200\
 		--datarate_experiment True
 done
