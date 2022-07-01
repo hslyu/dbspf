121424732bc6481f1f619b514261fc8d1ce09398
hslyu <hslyu4@gmail.com>
2022-07-01 08:29:42 +0000

    Adaptive gene number along time axis by using get_valid_users

--- comparison_scheme/no_GBS_iterative_ga_optimizer.py
+++ comparison_scheme/no_GBS_iterative_ga_optimizer.py
@@ -44,19 +44,17 @@ def proportion_array(arr: np.ndarray):
     else:
         return arr
 
-def print_gene_expression(list_ue_mode, list_ue_alpha, list_ue_power, \
-                          list_UBS_power, position_x, position_y, position_z):
-    print(f'{list_ue_mode = }')
-    for i in range(param.num_ue): 
-        print(f'sum(list_ue_alpha[{i}]) : {sum(list_ue_alpha[i]) : 4.2f}')
-    for i in range(param.num_ue):
-        print(f'list_ue_power[{i}] : {sum(list_ue_power[i]) : 4.2f}')
-    print(f'{sum(list_UBS_power) = :4.2f}')
-    print(f'position : ({position_x:4.2f}, {position_y:4.2f}, {position_z:4.2f})')
+#def print_gene_expression(list_ue_mode, list_ue_alpha, list_ue_power, \
+#                          list_UBS_power, position_x, position_y, position_z):
+#    print(f'{list_ue_mode = }')
+#    for i in range(param.num_ue): 
+#        print(f'sum(list_ue_alpha[{i}]) : {sum(list_ue_alpha[i]) : 4.2f}')
+#    for i in range(param.num_ue):
+#        print(f'list_ue_power[{i}] : {sum(list_ue_power[i]) : 4.2f}')
+#    print(f'{sum(list_UBS_power) = :4.2f}')
+#    print(f'position : ({position_x:4.2f}, {position_y:4.2f}, {position_z:4.2f})')
 
-def encode_gene_max_snr(UBS, GBS, list_ue, time_index):
-    valid_user_list = get_valid_user(list_ue, time_index)
-
+def encode_gene_max_snr(UBS, GBS, valid_user_list):
     list_ue_UBS_alpha = np.empty(param.num_subcarriers)
     for i in range(param.num_subcarriers): 
         max_snr = -99999999
@@ -67,7 +65,7 @@ def encode_gene_max_snr(UBS, GBS, list_ue, time_index):
                 max_user_index = user.id
         list_ue_UBS_alpha[i] = max_user_index
         
-    list_ue_power = np.ones(param.num_ue * param.num_subcarriers)
+    list_ue_power = np.ones(len(valid_user_list) * param.num_subcarriers)
     list_position = np.zeros(3)
 
     gene = np.concatenate((list_ue_UBS_alpha, list_ue_power, list_position))
@@ -78,15 +76,16 @@ def encode_gene_max_snr(UBS, GBS, list_ue, time_index):
     return gene
 
 def decode_gene(X):
+    valid_user_list = get_valid_user(list_ue, time_index)
     #alpha power theta pi radius
     a = param.num_subcarriers
-    b = a + param.num_ue * param.num_subcarriers
+    b = a + len(valid_user_list) * param.num_subcarriers
 
     list_ue_UBS_alpha_encoded = X[:a]
-    list_ue_UBS_alpha = (np.arange(param.num_ue).reshape([-1,1]) == list_ue_UBS_alpha_encoded).astype(int)
+    list_ue_UBS_alpha = (np.arange(len(valid_user_list)).reshape([-1,1]) == list_ue_UBS_alpha_encoded).astype(int)
 
     list_ue_power_encoded = X[a:b] # before softmax
-    list_ue_power_encoded = list_ue_power_encoded.reshape((param.num_ue,-1))
+    list_ue_power_encoded = list_ue_power_encoded.reshape((len(valid_user_list),-1))
     list_ue_power_encoded = np.multiply(list_ue_UBS_alpha, list_ue_power_encoded )
     list_ue_power = list_ue_power_encoded/sum(list_ue_power_encoded.flatten()) * param.max_ue_power_mW if sum(list_ue_power_encoded.flatten()) !=0 else list_ue_power_encoded
 #    list_ue_power = np.apply_along_axis(proportion_array, 1, list_ue_power_encoded) * param.max_ue_power_mW
@@ -122,10 +121,10 @@ def sol2rate(solution):
     UBS.calc_channel() 
 
     list_rate = []
-    for i, ue in enumerate(get_valid_user(list_ue, time_index)):
+    for i, user in enumerate(get_valid_user(list_ue, time_index)):
         # Find sum-rate 
         sumrate = 0
-        for j, subcarrier in enumerate(ue.list_subcarrier_UBS):
+        for j, subcarrier in enumerate(user.list_subcarrier_UBS):
             if list_ue_UBS_alpha[i,j] == 1:
                 carrier_rate = 0
                 # R^k_{n,R} in paper. eq (31)
@@ -153,7 +152,7 @@ def callback_generation(ga_instance):
     Generation = ga_instance.generations_completed
     Fitness = ga_instance.best_solution()[1]
     Change = ga_instance.best_solution()[1] - last_fitness
-#    print( f'{Generation = :02d},\t {Fitness = },\t\t {Change = }')
+#    print( f'{Generation = :02d}, {Fitness = :.4f}, {Change = :.4f}')
     last_fitness = ga_instance.best_solution()[1]
 
 parser = get_parser()
@@ -167,29 +166,9 @@ pi_bound         = {'low' : 0,'high' : 340, 'step': 20} # int
 #radius_bound     = {'low' : 0,'high' : param.uav_max_dist + 1e-8, 'step' : 1} # real
 radius_bound     = {'low' : 0,'high' : param.uav_max_dist, 'step':5} # int
 
-gene_space = []
-gene_type = []
-# alpha - UBS
-for _ in range(param.num_subcarriers):
-    gene_space.append(ue_alpha_bound)
-    gene_type.append(int)
-# UE power
-for _ in range(param.num_ue * param.num_subcarriers):
-    gene_space.append(ue_power_bound)
-    gene_type.append(int)
-
-gene_space.append(theta_bound)
-gene_type.append(int)
-gene_space.append(pi_bound)
-gene_type.append(int)
-gene_space.append(radius_bound)
-gene_type.append(int)
-
-num_generations = 10000 # Number of generations.
+num_generations = 20000 # Number of generations.
 num_parents_mating = 10 # Number of solutions to be selected as parents in the mating pool.
-sol_per_pop = 80 # Number of solutions in the population.
-#           subcarrier allocation          power allocation             theta pi radius
-num_genes = param.num_subcarriers + param.num_ue * param.num_subcarriers + 1 + 1 + 1
+sol_per_pop = 40 # Number of solutions in the population.
 
 idx_start = args.index_start
 idx_end = args.index_end
@@ -205,8 +184,32 @@ for i in range(idx_start, idx_end):#{{{
     global time_index 
     for time_index in range(param.num_timeslots):#{{{
         start = time.time()
-        if get_valid_user(list_ue, time_index) == []:
+        valid_user_list = get_valid_user(list_ue, time_index)
+        if valid_user_list == []:
             continue
+
+        gene_space = []
+        gene_type = []
+        # alpha - UBS
+        ue_alpha_bound   = {'low' : 0,'high' : len(valid_user_list) + 1} # int
+        for _ in range(param.num_subcarriers):
+            gene_space.append(ue_alpha_bound)
+            gene_type.append(int)
+        # UE power
+        for _ in range(len(valid_user_list) * param.num_subcarriers):
+            gene_space.append(ue_power_bound)
+            gene_type.append(int)
+
+        gene_space.append(theta_bound)
+        gene_type.append(int)
+        gene_space.append(pi_bound)
+        gene_type.append(int)
+        gene_space.append(radius_bound)
+        gene_type.append(int)
+
+        #           subcarrier allocation          power allocation             theta pi radius
+        num_genes = param.num_subcarriers + len(valid_user_list) * param.num_subcarriers + 1 + 1 + 1
+        print(f"{num_genes = }")
 
         last_fitness = 0
 
@@ -220,11 +223,11 @@ for i in range(idx_start, idx_end):#{{{
                                crossover_type = 'single_point',
                                gene_type = copy.deepcopy(gene_type),
                                gene_space = gene_space,
-                               stop_criteria = ["saturate_750"]
+                               stop_criteria = ["saturate_1000"]
                                )
         
         # Running the GA to optimize the parameters of the function.
-        ga_instance.population[0] = encode_gene_max_snr(UBS, GBS, list_ue, time_index)
+        ga_instance.population[0] = encode_gene_max_snr(UBS, GBS, valid_user_list)
         ga_instance.run()
 
         # After the generations complete, some plots are showed that summarize the how the outputs/fitenss values evolve over generations.
