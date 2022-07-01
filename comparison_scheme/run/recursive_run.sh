18141648f132afd798616fc3cf80dcfef7030eac
hslyu <hslyu4@gmail.com>
2022-07-01 19:19:20 +0000

    modify code to use the datarate

--- comparison_scheme/run/recursive_run.sh
+++ comparison_scheme/run/recursive_run.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ];
+if [ "$#" -ne 3 ];
 then 
-	echo "usage: $0 <NUM_EXP> <START_INDEX>"
+	echo "usage: $0 <NUM_EXP> <START_INDEX> <DATARATE>"
 	exit
 fi	
 
 sess="PF"
 
-NUM_SESS=10
 NUM_EXP=$1
 START=$2
+DATARATE=$3
+
+NUM_SESS=10
 WINDOWS=$(seq 0 $NUM_SESS)
 
-for datarate in $(seq 0 2 10)
+for window in $WINDOWS
 do
-	for window in $WINDOWS
-	do
-		if [ $window -eq 0 ]; then
-			continue
-		fi
-		END=`expr $START + $NUM_EXP`
-		tmux send-keys -t $sess:$window "python no_GBS_iterative_ga_optimizer.py --index_start $START --index_end $END --datarate $datarate" Enter
-		START=$END
-	done
-done
-
-for datarate in $(seq 1 2 9)
-do
-	for window in $WINDOWS
-	do
-		if [ $window -eq 0 ]; then
-			continue
-		fi
-		END=`expr $START + $NUM_EXP`
-		tmux send-keys -t $sess:$window "python no_GBS_iterative_ga_optimizer.py --index_start $START --index_end $END --datarate $datarate" Enter
-		START=$END
-	done
+	if [ $window -eq 0 ]; then
+		continue
+	fi
+	END=`expr $START + $NUM_EXP`
+	tmux send-keys -t $sess:$window "python no_GBS_iterative_ga_optimizer.py --index_start $START --index_end $END --datarate $DATARATE" Enter
+	START=$END
 done
