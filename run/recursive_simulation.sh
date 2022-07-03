267838f78b6020dee14f82ade279e296a55d5069
hslyu <hslyu4@gmail.com>
2022-07-03 00:22:54 +0000

    .

--- run/recursive_simulation.sh
+++ run/recursive_simulation.sh
@@ -8,17 +8,29 @@ fi
 START=$1
 END=$2
 
-for num_user in $(seq 10 10 80)
-do
-	for datarate in $(seq 0 15)
-	do
-		for depth in $(seq 1 9)
-		do
-			result_dir="./result/datarate_$datarate/result-depth_$depth-user_$num_user"
-			read test <<< $(python iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir)
-			echo "$test"
-		done
-		echo "------------------------------------------------------"
-	done
-done
+#for num_user in $(seq 10 10 80)
+#do
+#	for datarate in $(seq 0 15)
+#	do
+#		for depth in $(seq 1 7)
+#		do
+#			result_dir="./result/datarate_$datarate/result-depth_$depth-user_$num_user"
+#			read test <<< $(python iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir)
+#			echo "$test"
+#		done
+#		echo "------------------------------------------------------"
+#	done
+#done
 
+
+num_user=20
+for datarate in $(seq 0 10)
+do
+	for depth in $(seq 1 8)
+	do
+		result_dir="./result/datarate_$datarate/result-depth_$depth-user_$num_user"
+		read test <<< $(python iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir)
+		echo "$test"
+	done
+	echo "------------------------------------------------------"
+done
