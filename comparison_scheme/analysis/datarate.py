5d34b5cd3f18771ef5eafe80c6d18494e97032b4
hslyu <hslyu4@gmail.com>
2022-07-06 17:23:42 +0000

    change folder name

--- comparison_scheme/analysis/datarate.py
+++ comparison_scheme/analysis/datarate.py
@@ -18,7 +18,7 @@ sys.path.insert(0, parent_dir)
 import system_model as sm
 
 num_env=150
-def parse(root: str="/home/hslyu/storage/result_twc21/tw20_user20"):
+def parse(root: str="/home/hslyu/storage/result_twc21_07_06/tw20_user20"):
     list_pf = []
 
     for i in range(0,11):
--- comparison_scheme/analysis/pf.py
+++ comparison_scheme/analysis/pf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, parent_dir)
 import system_model as sm
 
 num_env=150
-def parse(root: str="/home/hslyu/storage/result_twc21/tw20_user20/datarate_0"):
+def parse(root: str="/home/hslyu/storage/result_twc21_07_06/tw20_user20/datarate_0"):
     list_pf = np.zeros(20)
 
     for j in range(num_env):
@@ -47,7 +47,7 @@ if __name__=="__main__":
     plt.ylabel("pf")
     for d in range(0,11):
         print(d)
-        root = f"/home/hslyu/storage/result_twc21/tw20_user20/datarate_{d}"
+        root = f"/home/hslyu/storage/result_twc21_07_06/tw20_user20/datarate_{d}"
         plt.plot(parse(root), label=f"datarate_{d}")
         
     plt.legend(loc='best')
