2505a11e0b1cb3d5f4062356626fb9c76cf4db08
hslyu <hslyu4@gmail.com>
2022-07-08 15:37:33 +0000

    restore prev_setting

--- run/recursive_simulation.sh
+++ run/recursive_simulation.sh
@@ -1,30 +1,22 @@
+
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-	echo "First initialize tmux session using initialize.sh"
-	echo "usage: $0 <NUM_EXP> <START_INDEX> <datarate>"
+if [ "$#" -ne 2 ]; then
+	echo "Usage: ../run/recursive_simulation.sh <index_start> <index_end>"
 	exit
 fi
 
-NUM_EXP=$1
-START=$2
-datarate=$3
-sess="PF$3"
-
-NUM_SESS=10
-WINDOWS=$(seq 0 $NUM_SESS)
+START=$1
+END=$2
 
 num_user=20
-for window in $WINDOWS
+for datarate in $(seq 0 4)
 do
-	if [ $window -eq 0 ]; then
-		continue
-	fi
-	END=`expr $START + $NUM_EXP`
 	for depth in $(seq 1 7)
 	do
 		result_dir="result/datarate_$datarate/user_$num_user/depth_$depth"
-		tmux send-keys -t $sess:$window "python ~/dbspf/iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir" Enter
+		read test <<< $(python iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir)
+		echo "$test"
 	done
-	START=$END
+	echo "------------------------------------------------------"
 done
