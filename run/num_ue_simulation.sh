2188c370c00c0db2de6146437f6c918d4ae94a8e
hslyu <hslyu4@gmail.com>
2022-07-13 19:47:03 +0000

    modify shell execution code

--- run/num_ue_simulation.sh
+++ run/num_ue_simulation.sh
@@ -9,17 +9,6 @@ fi
 START=$1
 END=$2
 
-#num_user=20
-#for datarate in $(seq 15 15)
-#do
-#	for depth in $(seq 1 2 5)
-#	do
-#		result_dir="result/datarate_$datarate/user_$num_user/depth_$depth"
-#		python iterative_simulation.py -t $depth --index_start $START --index_end $END --num_user $num_user --datarate $datarate --result_path $result_dir
-#	done
-#	echo "------------------------------------------------------"
-#done
-
 datarate=5
 for num_user in 10 80;
 do
