adb9620a33a0edf856510968f99aa49264b70691
hslyu <hslyu4@gmail.com>
2022-07-13 19:19:33 +0000

    We don't need old result dir

--- run/run_iterative_simulation.sh
+++ run/run_iterative_simulation.sh
@@ -5,5 +5,4 @@ if [ "$#" -ne 5 ]; then
 	exit
 fi
 num_user=$4
-result_dir="./result/result-depth_$1-user_$num_user"1
-python iterative_simulation.py -t $1 --index_start $2 --index_end $3 --num_user $num_user --datarate $5 --result_path $result_dir
+python iterative_simulation.py -t $1 --index_start $2 --index_end $3 --num_user $num_user --datarate $5 
