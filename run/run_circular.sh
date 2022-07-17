0904ee4c2d010019c691bc1d50280fa1045db7fb
hslyu <hslyu4@gmail.com>
2022-07-17 13:32:55 +0000

    add new code for fixed and circular trajectory

---
+++ run/run_circular.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$#" -ne 5 ]; then
+	echo "Usage: ../run/run_iterative_simulation.sh <depth> <index_start> <index_end> <num_user> <datarate>"
+	exit
+fi
+num_user=$4
+python iterative_simulation.py -t $1 --index_start $2 --index_end $3 --num_user $num_user --datarate $5 --mode circular
